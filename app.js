ac4633a279ea655c1806f97c2d74f2ed3103cc71
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-07-19 12:22:29 +0100

    intro

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const firstName = document.querySelector('#firstname');
 const lastName = document.querySelector('#lastname');
 const emailAdd = document.querySelector('#email');
 const passWord = document.querySelector('#new-password');
+const firstNameError = document.querySelector('#firstname-error');
+const lastNameError = document.querySelector('#lastname-error');
+const emailError = document.querySelector('#email-error');
+const passwordError = document.querySelector('#new-password-error');
+
 
 const btnSubmit = document.querySelector('.btn');
 
 btnSubmit.addEventListener('submit', (e)=>{
     e.preventDefault();
+    
 });
 
 
@@ -23,3 +29,96 @@ const isPasswordSecure = (password) => {
     const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
     return re.test(password);
 };
+
+
+
+const checkFirstName =()=>{
+    let valid = false;
+    const min = 3;
+    const max = 25;
+    const firstname = firstName.value.trim()
+
+    if(!isRequired(firstname)){
+        firstNameError.innerText = 'FirstName cannot be blank.'
+        firstNameError.style.color ='red';
+        firstName.style.border = '1.8px solid red'
+
+
+    }
+    else if(!isBetween(firstname.length, min, max)){
+        firstNameError.innerText = 'Firstname must be between ${min} and ${max} characters.'
+        firstNameError.style.color ='red';
+        firstName.style.border = '1.8px solid red'
+    }
+    else{
+        valid = true;
+    }
+
+}
+
+const checkLastName =()=>{
+    let valid = false;
+    const min = 3;
+    const max = 25;
+    const lastname = lastName.value.trim()
+
+    if(!isRequired(lastname)){
+        lastNameError.innerText = 'lastName cannot be blank.'
+        lastNameError.style.color ='red';
+        lastName.style.border = '1.8px solid red'
+
+
+    }
+    else if(!isBetween(lastname.length, min, max)){
+        lastNameError.innerText = 'Lastname must be between ${min} and ${max} characters.'
+        lastNameError.style.color ='red';
+        lastName.style.border = '1.8px solid red'
+    }
+    else{
+        valid = true;
+    }
+
+}
+
+const checkEmail= () =>{
+    let valid = false;
+    const email = emailAdd.value.trim()
+    if(!isRequired(email)){
+        emailError.innerText = 'email cannot be blank';
+        emailError.style.color = 'red';
+        emailAdd.style.border = '1.8px solid red';
+    }
+    else if(!isEmailValid(email)){
+        emailError.innerText = 'provide valid email';
+        emailError.style.color = 'red';
+        emailAdd.style.border = '1.8px solid red';
+
+    }
+    else{
+        valid = true;
+    }
+
+}
+
+const checkPassword = () =>{
+    let valid = false;
+    const password = passWord.value.trim();
+    if(!isRequired(password)){
+        passwordError.innerText = 'password cannot be blank';
+        passwordError.style.color = 'red';
+        passWord.style.border = '1.8px solid red';
+
+    }
+    else if(!isPasswordSecure(password)){
+        passwordError.innerText = 'password must include 8 characters, at least one uppercase, lowercase, number and special character';
+        passwordError.style.color = 'red';
+        passWord.style.border = '1.8px solid red';
+
+    }
+
+    else{
+        valid = true;
+    }
+
+    
+}
